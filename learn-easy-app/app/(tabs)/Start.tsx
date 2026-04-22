8fe2fbf8b5bee2e2eadd43984c95003ea7712529
Sharif Uddin <sharifuddin191100@gmail.com>
2026-04-23 00:47:18 +0200

    Action Bar hinzugefügt entgültig

---
+++ learn-easy-app/app/(tabs)/Start.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { HelloWave } from '@/components/hello-wave';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Start</ThemedText>
+        <HelloWave />
+      </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    marginTop: 80,
+    marginRight: 150,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+});
