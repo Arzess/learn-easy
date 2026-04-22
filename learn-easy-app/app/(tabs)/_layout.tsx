5c0a1a9155046fc95fb925e5a1d36966827f7c0a
Sharif Uddin <sharifuddin191100@gmail.com>
2026-04-22 23:49:09 +0200

    ich habe die Action Bar hinzugefügt

--- learn-easy-app/app/(tabs)/_layout.tsx
+++ learn-easy-app/app/(tabs)/_layout.tsx
@@ -9,25 +9,44 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const activeColor = colorScheme && colorScheme in Colors ? Colors[colorScheme as keyof typeof Colors].tint : Colors.light.tint;
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: activeColor,
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
+        name="Start"
         options={{
-          title: 'Home',
+          title: 'Start',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="Suche"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Suche',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="Lernen"
+        options={{
+          title: 'Lernen',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="book.fill" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="Account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
     </Tabs>
